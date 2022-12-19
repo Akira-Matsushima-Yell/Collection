857784379a7454fd5bb4833317031dce993e3957
Akira_Matsushima <119549568+Akira-Matsushima-Yell@users.noreply.github.com>
2022-12-20 01:10:19 +0900

    課題提出

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -1,35 +1,32 @@
 package practice;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 
 public class Chapter5 {
-	public static LinkedHashMap<LocalDate, String> sortMapByKey(Map<LocalDate, String> map) {
-		List<Map.Entry<LocalDate, String>> entries = new LinkedList<>(map.entrySet());
-		Collections.sort(entries, (o1, o2) -> o1.getKey().compareTo(o2.getKey()));
-
-		LinkedHashMap<LocalDate, String> result = new LinkedHashMap<>();
-		for (Map.Entry<LocalDate, String> entry : entries) {
-			result.put(entry.getKey(), entry.getValue());
-		}
-		return result;
-	}
-
 	public static void main(String[] args) {
-		Map<LocalDate, String> map1 = new LinkedHashMap<>();
-		map1.put(LocalDate.of(2021, 10, 21), "牛乳を買う");
-		map1.put(LocalDate.of(2021, 9, 15), "○○社面談");
-		map1.put(LocalDate.of(2021, 12, 4), "手帳を買う");
-		map1.put(LocalDate.of(2021, 8, 10), "散髪に行く");
-		map1.put(LocalDate.of(2021, 11, 9), "スクールの課題を解く");
+		List<Task> map1 = new ArrayList<>();
 
-		Map<LocalDate, String> result = sortMapByKey(map1);
-		for (Map.Entry<LocalDate, String> entry : result.entrySet()) {
-			System.out.println(entry.getKey() + " : " + entry.getValue());
+		map1.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う。"));
+		map1.add(new Task(LocalDate.of(2021, 9, 15), "○○社面談。"));
+		map1.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う。"));
+		map1.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く。")); 
+		map1.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く。"));
+
+		Collections.sort(map1, new Comparator<Task>() {
+			@Override
+			public int compare(Task t1, Task t2) {
+				return t1.getDay().compareTo(t2.getDay());
+			}
+		});
+
+		for (Task task1 : map1) {
+			System.out.println(task1.getDay() + ":" + task1.getTask());
 		}
+
 	}
+
 }
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -6,9 +6,15 @@ public class Task{
 	public String task;
 	public LocalDate day;
 	
-	public Task( LocalDate day, String task) {
+	public  Task( LocalDate day, String task) {
 		this.task =task;
 		this.day = day;
+	}
+	public LocalDate getDay() {
+		return day;
+	}
+	public String getTask() {
+		return task;
 	}
 
 }
